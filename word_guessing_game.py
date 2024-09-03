6bab06c724cd7ea0c35af259fb24bae397be0138
Sina Arasteh <sinna.arasteh@gmail.com>
2024-09-03 17:18:41 +0330

    Choose the random word

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -3,3 +3,6 @@ import random
 name = input("What is your name? ")
 print(f"Good luck {name}!")
 
+words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics', 'player', 'condition', 'reverse', 'water', 'board', 'geeks']
+word = random.choice(words)
+
