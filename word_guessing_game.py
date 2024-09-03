253818f4ecf46c38c1b7f22505fa002070bb6c82
Sina Arasteh <sinna.arasteh@gmail.com>
2024-09-03 17:28:39 +0330

    Add the user's number of guesses message

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,7 +1,7 @@
 import random
 
 name = input("What is your name? ")
-print(f"Good luck {name}!")
+print(f"Good luck '{name}'!")
 
 words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics', 'player', 'condition', 'reverse', 'water', 'board', 'geeks']
 word = random.choice(words)
@@ -9,4 +9,6 @@ word = random.choice(words)
 print("Here is the chosen word:")
 for char in word:
     print("_")
+print("")
 print("You have got 12 guesses.")
+
