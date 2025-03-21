f34e15171560c68d2e970d6f842b6a3b05871a93
Sina Arasteh <sinna.arasteh@gmail.com>
2025-03-22 00:37:43 +0330

    Modify the names of some of the variables

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,28 +1,28 @@
 import random
 
-name = input("What is your name? ").lower().capitalize()
-print(f"Good luck '{name}'!\n")
+player_name = input("What is your name? ").lower().capitalize()
+print(f"Good luck '{player_name}'!\n")
 
 words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics', 'player', 'condition', 'reverse', 'water', 'board', 'geeks']
-word = random.choice(words)
+selected_word = random.choice(words)
 
 print("Here is the random word:")
-for char in word:
+for char in selected_word:
     print("_")
 print("")
-print("You have got 12 chances.\n")
+print("You have 12 chances to guess the correct characters.\n")
 
 def word_guessing_game():
     chances = 12
-    guesses = []
+    guessed_chars = []
     loop = 0
     while chances > 0:
-        if set(guesses) == set(word):
-            return f"Congratulations, '{name}'. You win the game.\n The word is '{word}'.\n"
+        if set(guessed_chars) == set(selected_word):
+            return f"Congratulations, '{player_name}'. You won the game.\n The word is '{selected_word}'.\n"
         
         loop += 1
         if loop > 1:
-            print(f"You still have got '{chances}' chances.")
+            print(f"You still have '{chances}' chances.")
         guess = input("guess a character: ").lower()
 
         if len(guess) > 1:
@@ -33,10 +33,10 @@ def word_guessing_game():
             continue
 
         chances -= 1
-        if guess in list(word):
-            guesses.append(guess)
-            for char in word:
-                if char in guesses:
+        if guess in list(selected_word):
+            guessed_chars.append(guess)
+            for char in selected_word:
+                if char in guessed_chars:
                     print(char)
                 else:
                     print("_")
