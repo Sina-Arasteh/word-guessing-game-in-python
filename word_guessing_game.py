292c4c399911cd84c46766bc47bdaa7462d182d1
Sina Arasteh <sinna.arasteh@gmail.com>
2025-03-22 22:22:06 +0330

    Improve the user interfase

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,49 +1,55 @@
 import random
 
-player_name = input("What is your name? ").lower().capitalize()
-print(f"Good luck '{player_name}'!\n")
+print('Welcome to "Word Guessing Game"')
+player_name = input("What's your name? ").lower().capitalize()
+print("")
 
 words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics', 'player', 'condition', 'reverse', 'water', 'board', 'geeks']
 selected_word = random.choice(words)
 
-print("Here is the random word:")
+print("Here's the selected random word (The word is in lowercase):")
 for char in selected_word:
-    print("_")
+    print("-")
 print("")
-print("You have 12 chances to guess the correct characters.\n")
+
+chances = 12
+print(f'Dear "{player_name}", You have 12 chances to guess the correct characters.')
 
 def word_guessing_game():
-    chances = 12
-    guessed_chars = []
+    global chances
+    guessed_chars_correct = []
+    guessed_chars_all = []
     loop = 0
     while chances > 0:
-        if set(guessed_chars) == set(selected_word):
-            return f"Congratulations, '{player_name}'. You won the game.\n The word is '{selected_word}'.\n"
+        if set(guessed_chars_correct) == set(selected_word):
+            return f"Congratulations, '{player_name}'. You won the game.\nThe word is '{selected_word}'.\n"
         
+        if loop > 0:
+            print(f"Guessed characters: {guessed_chars_all}")
+            print(f"Your chances: {chances}")
+            
         loop += 1
-        if loop > 1:
-            print(f"You still have '{chances}' chances.")
         guess = input("guess a character: ").lower()
 
         if len(guess) > 1:
             print(f"Please enter only ONE character. You entered {len(guess)} characters.\n")
             continue
         if guess.isalpha() == False:
-            print(f"{guess} is not an alphabetic character. Please enter an ALPHABETIC character.\n")
+            print(f'"{guess}" is not an alphabetic character. Please enter an ALPHABETIC character.\n')
             continue
 
         chances -= 1
+        guessed_chars_all.append(guess)
         if guess in list(selected_word):
-            guessed_chars.append(guess)
-            for char in selected_word:
-                if char in guessed_chars:
-                    print(char)
-                else:
-                    print("_")
-            print("")
-        else:
-            print("Wrong.\n")
-    return "Game over.\n"
+            guessed_chars_correct.append(guess)
+
+        for char in selected_word:
+            if char in guessed_chars_correct:
+                print(char)
+            else:
+                print("-")
+        print("")
+
+    return "Unfortunately, you lost the game.\nGame over.\n"
 
 print(word_guessing_game())
-    
